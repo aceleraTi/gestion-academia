8e33d4b4090ef3a691783a5ff264bbbf7c927b3c
Nelson M <nelsondmondragon@gmail.com>
2023-06-10 11:57:16 -0500

    primera version api-academia

---
+++ src/test/java/com/acelerati/gestionacademia/ApiAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acelerati.gestionacademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
