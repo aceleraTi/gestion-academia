899cf64991211f68fcd825daefaa8722ba77ba59
vimabo <vimabo2908@gmail.com>
2023-06-21 18:37:52 -0500

    Feat: Modificacion de archivos por ajustes del requisito.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,38 +1,29 @@
 INSERT INTO nivel_educativo(nombre,descripcion)
-values ("educación primaria", " escuela primaria (grados 1 a 6)"),
-("educación secundaria"," escuela secundaria (grados 7 a 11)"),
-("educación superior","niveles: pregrado y posgrado")
-
-;
+values ('Educación Primaria', 'Escuela primaria (grados 1 a 6)'),
+('Educación Secundaria', 'Escuela secundaria (grados 7 a 11)'),
+('Educación Superior','Niveles: pregrado y posgrado');
 
 
 INSERT INTO programa_academico(nombre,descripcion,id_nivel_educativo)
 values
-('IngenieriaSistemas','Descripcion ingenieria de sistemas, descripcion', 1),
-('IngenieriaSistema2s','2Descripcion ingenieria de sistemas, descripcion2', 2)
-;
-
-INSERT INTO programa_academico(nombre,descripcion,id_nivel_educativo,id_director)
-values
-('IngenieriaSistemas5','Descripcion ingenieria de sistemas, descripcion', 1,1)
-;
+('Ingenieria de Sistemas','Programa de Ingenieria de Sistemas', 3),
+('Ingenieria Electronica','Programa de ingenieria electronica', 3),
+('Ingenieria Ambiental','Programa de ingenieria ambiental', 2);
 
 
 INSERT INTO pensum(annio, id_programa_academico)
 values
 (2022,1),
 (2023,1),
-(2025,1)
-;
+(2025,1);
 
 INSERT INTO materia (nombre, descripcion, id_pensum)
 values
-("Calculo 1", "Matematicas descripcion testttt", 1),
-("Estadistica 1", "Estadistica descripcion testttt", 2)
-;
+('Calculo 1', 'Matematicas Calculo integral primera parte', 1),
+('Estadistica 1', 'Estadistica de datos', 2);
 
 
 INSERT INTO materia (nombre, descripcion, id_pensum,id_materia_prerequisito)
 values
-("Calculo 2", "Matematicas descripcion testttt", 1,1)
+('Calculo 2', 'Matematicas Calculo integral segunda parte', 1,1)
 ;
